0b59aabf1d9875f41f755d0f0ceff591c958f362
Harsh Gupta <harshgupta3341@gmail.com>
2024-03-20 00:48:12 +0530

    removed brochure

--- components/Home/WhySponsor.js
+++ components/Home/WhySponsor.js
@@ -188,35 +188,6 @@ const WhySponsor = () => {
             <span className="text"> Sponsor Us </span>
           </a>
         </button>
-        <button type="button" className="Btn zinc-bg">
-          <a href="https://bit.ly/Brochure-HTS">
-            <div className="gradient-mask blue-bg"></div>
-            <svg
-              name="arrow-down-circle-outline"
-              xmlns="http://www.w3.org/2000/svg"
-              className="ionicon icon"
-              viewBox="0 0 512 512"
-            >
-              <path
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="32"
-                d="M176 262.62L256 342l80-79.38M256 330.97V170"
-              />
-              <path
-                d="M256 64C150 64 64 150 64 256s86 192 192 192 192-86 192-192S362 64 256 64z"
-                fill="none"
-                stroke="currentColor"
-                strokeMiterlimit="10"
-                strokeWidth="32"
-              />
-            </svg>
-
-            <span className="text">Brochure</span>
-          </a>
-        </button>
       </div>
 
       <div className="aconst3">
